622388c9f3e050042f8b5321e52a3f1d0ace3bb4
elianismedina <elianismedina05@outlook.com>
2024-09-07 18:54:13 -0500

    added source and type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,12 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div>
-          <video
-            src="https://res.cloudinary.com/dxa54qfxx/video/upload/v1725752193/invite_ab744a.mp4"
-            autoPlay
-            loop
-            muted
-            className="w-full rounded-lg"
-          />
+          <video>
+            <source
+              src="https://res.cloudinary.com/dxa54qfxx/video/upload/v1725752193/invite_ab744a.mp4"
+              type="video/mp4"
+            />
+          </video>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
