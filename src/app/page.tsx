66082bd9f4ae2d88d8fd2162c7230795b03436dd
elianismedina <elianismedina05@outlook.com>
2024-09-07 19:09:02 -0500

    changed video

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,17 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div>
-          <video>
+          <video
+            width={300}
+            height={300}
+            controls
+            preload="auto"
+            autoPlay
+            loop
+            muted
+            playsInline
+            className="rounded-lg"
+          >
             <source
               src="https://res.cloudinary.com/dxa54qfxx/video/upload/v1725752193/invite_ab744a.mp4"
               type="video/mp4"
