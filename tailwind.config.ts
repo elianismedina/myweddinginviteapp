652912d18d712288a5a4310b91964261574770ea
elianismedina <elianismedina05@outlook.com>
2024-09-08 17:32:15 -0500

    added color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,23 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
+        transparent: "transparent",
+        current: "currentColor",
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: {
+          50: "#FFFEFC",
+          100: "#FFFDFA",
+          200: "#FFFBF5",
+          300: "#FFF8F0",
+          400: "#FFEEE3",
+          500: "#FFD4C5",
+          600: "#E6BEAE",
+          700: "#BF8B78",
+          800: "#995E4D",
+          900: "#733B2C",
+          950: "#4A1D12",
+        },
       },
     },
   },
