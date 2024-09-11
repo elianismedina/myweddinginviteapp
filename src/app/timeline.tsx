44c7c10edc7a383a382163aa6c2f877b153e2cfe
elianismedina <elianismedina05@outlook.com>
2024-09-11 17:06:38 -0500

    changed timeline

--- src/app/timeline.tsx
+++ src/app/timeline.tsx
@@ -32,8 +32,6 @@ const TimelineSection = () => {
                 fontSize: "1.5rem",
               }}
             >
-              <h3 className="font-semibold capitalize">{item.event}</h3>
-              <p className="font-normal !mt-0">{item.location}</p>
               <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
                 {item.time}
               </p>
--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,41 +1,34 @@
 import React from "react";
 import { PiChurchLight } from "react-icons/pi";
+import { FaChampagneGlasses } from "react-icons/fa6";
+import { MdFoodBank } from "react-icons/md";
+import { LuPartyPopper } from "react-icons/lu";
+import { FaGlassMartiniAlt } from "react-icons/fa";
 
 export const itineraryData = [
   {
-    event: "Breakfast",
-    location: "Hotel",
-    time: "7:00 AM",
+    event: "Ceremonia",
+    time: "5:00 PM",
     icon: React.createElement(PiChurchLight, null),
   },
   {
-    event: "Morning Hike",
-    location: "Mountains",
-    time: "9:00 AM",
-    icon: React.createElement(PiChurchLight, null),
-  },
-  {
-    event: "Lunch",
-    location: "Cafe",
-    time: "12:00 PM",
-    icon: React.createElement(PiChurchLight, null),
-  },
-  {
-    event: "Afternoon Swim",
-    location: "Beach",
-    time: "3:00 PM",
-    icon: React.createElement(PiChurchLight, null),
-  },
-  {
-    event: "Dinner",
-    location: "Restaurant",
+    event: "Coctel de bienvenida",
     time: "6:00 PM",
-    icon: React.createElement(PiChurchLight, null),
+    icon: React.createElement(FaGlassMartiniAlt, null),
   },
   {
-    event: "Evening Drinks",
-    location: "Bar",
+    event: "Brindis",
+    time: "7:00 PM",
+    icon: React.createElement(FaChampagneGlasses, null),
+  },
+  {
+    event: "Comida",
+    time: "8:00 PM",
+    icon: React.createElement(MdFoodBank, null),
+  },
+  {
+    event: "Rumba",
     time: "9:00 PM",
-    icon: React.createElement(PiChurchLight, null),
+    icon: React.createElement(LuPartyPopper, null),
   },
 ] as const;
