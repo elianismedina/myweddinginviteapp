9ba576e8ad0bacc83334d5ee1490bda578f2f0f9
elianismedina <elianismedina05@outlook.com>
2024-09-11 08:03:45 -0500

    changed email template

--- src/emails/contact-form-email.tsx
+++ src/emails/contact-form-email.tsx
@@ -24,14 +24,14 @@ export default function ContactFormEmail({
   return (
     <Html>
       <Head />
-      <Preview>New message from your portfolio site</Preview>
+      <Preview>Nuevo mensaje</Preview>
       <Tailwind>
-        <Body className="bg-gray-100 text-black">
+        <Body className="bg-primary-500 text-primary-900">
           <Container>
-            <Section className="bg-white borderBlack my-10 px-10 py-4 rounded-md">
+            <Section className="bg-primary-500 borderBlack my-10 px-10 py-4 rounded-md">
               <Heading className="leading-tight">
-                Hola Carlos & Elizabeth, un invitado ha confirmado su asistencia
-                a tu boda
+                Hola Carlos & Elizabeth. <br />
+                Un invitado ha confirmado su asistencia a tu boda
               </Heading>
               <Text>
                 El nombre del invitado que confirmó su asistencia es:{" "}
@@ -39,6 +39,15 @@ export default function ContactFormEmail({
               </Text>
               <Hr />
               <Text>El teléfono del invitado es: {senderPhone}</Text>
+              <Hr />
+              <Text>
+                Por favor, revisa la información y confirma la asistencia del
+                invitado.
+              </Text>
+              <Hr />
+              <Text>
+                Saludos, el equipo de soporte de la plataforma de confirmación
+              </Text>
             </Section>
           </Container>
         </Body>
