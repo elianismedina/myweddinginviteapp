a62c7454a10a5a8e29cf5a617c1b58a0fc0c58f5
elianismedina <elianismedina05@outlook.com>
2024-09-13 17:22:40 -0500

    added men dresscode

--- src/app/ceremonia.tsx
+++ src/app/ceremonia.tsx
@@ -2,51 +2,58 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { FaMapLocationDot } from "react-icons/fa6";
+import localFont from "next/font/local";
+
+const raleway = localFont({
+  src: "./fonts/Raleway-VariableFont_wght.ttf",
+});
 
 const CeremoniaSection = () => {
   return (
     <>
-      <div className="flex flex-col items-center justify-center">
-        <h1 className="text-4xl font-bold text-center text-primary-900 mb-4">
-          Ceremonia
-        </h1>
-        <div className="flex flex-col gap-2 mb-6">
-          <h2 className="text-lg text-center text-primary-900 font-semibold">
-            Lugar:
-          </h2>
-          <p className="text-lg text-center text-primary-900">
-            Iglesia Catedral de <br /> Santa Catalina de Alejandría
-          </p>
+      <div className={raleway.className}>
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-4xl font-bold text-center text-primary-900 mb-4">
+            Ceremonia
+          </h1>
+          <div className="flex flex-col gap-2 mb-6">
+            <h2 className="text-lg text-center text-primary-900 font-semibold">
+              Lugar:
+            </h2>
+            <p className="text-lg text-center text-primary-900">
+              Iglesia Catedral de <br /> Santa Catalina de Alejandría
+            </p>
+          </div>
+          <div className="flex flex-col gap-2 mb-6">
+            <h2 className="text-lg text-center text-primary-900 font-semibold">
+              Dirección:
+            </h2>
+            <p className="text-lg text-center text-primary-900">
+              Calle de los Santos de Piedra,
+              <br /> Cra. 4, Cartagena, Bolívar
+            </p>
+          </div>
         </div>
-        <div className="flex flex-col gap-2 mb-6">
-          <h2 className="text-lg text-center text-primary-900 font-semibold">
-            Dirección:
-          </h2>
-          <p className="text-lg text-center text-primary-900">
-            Calle de los Santos de Piedra,
-            <br /> Cra. 4, Cartagena, Bolívar
-          </p>
+        <div className="flex flex-col items-center justify-center p-4">
+          <Image
+            src="https://res.cloudinary.com/dxa54qfxx/image/upload/v1726016196/croppediglesia5_t9he1p.png"
+            alt="Iglesia"
+            quality={100}
+            width={600}
+            height={800}
+            objectFit="contain"
+          />
         </div>
-      </div>
-      <div className="flex flex-col items-center justify-center p-4">
-        <Image
-          src="https://res.cloudinary.com/dxa54qfxx/image/upload/v1726016196/croppediglesia5_t9he1p.png"
-          alt="Iglesia"
-          quality={100}
-          width={600}
-          height={800}
-          objectFit="contain"
-        />
-      </div>
-      <div className="flex flex-col items-center justify-center">
-        <FaMapLocationDot className="text-4xl text-primary-900" />
-        <Link
-          href="https://maps.app.goo.gl/U7cmhhMcFryFT67w8"
-          className="flex items-center justify-center p-4 text-lg text-primary-900 font-semibold underline"
-          target="_blank"
-        >
-          Toca para ir a la ubicación
-        </Link>
+        <div className="flex flex-col items-center justify-center">
+          <FaMapLocationDot className="text-4xl text-primary-900" />
+          <Link
+            href="https://maps.app.goo.gl/U7cmhhMcFryFT67w8"
+            className="flex items-center justify-center p-4 text-lg text-primary-900 font-semibold underline"
+            target="_blank"
+          >
+            Toca para ir a la ubicación
+          </Link>
+        </div>
       </div>
     </>
   );
--- src/app/dresscode.tsx
+++ src/app/dresscode.tsx
@@ -8,34 +8,61 @@ const raleway = localFont({
 
 const DresscodeSection = () => {
   return (
-    <div className={raleway.className}>
-      <div className="mb-8 mt-8 p-4">
-        <h1 className="text-4xl font-bold text-center text-primary-900 mb-4">
-          Código de vestimenta
-        </h1>
-        <div className="flex flex-col items-center justify-center gap-4">
-          <div>
-            <h2 className="font-semibold text-primary-900 text-2xl mb-4 text-center">
-              Mujeres
-            </h2>
-            <p className="text-primary-900 text-center">
-              Vestido largo de un solo color, tacones comodos.
-              <br /> Se reserva el uso de estampados y los colores blanco y
-              magenta.
-            </p>
-          </div>
-          <div>
-            <Image
-              src="https://res.cloudinary.com/dxa54qfxx/image/upload/v1726263127/mujerescropped_gxztzz.png"
-              alt="Mujeres"
-              width={500}
-              height={500}
-              className="mb-4"
-            />
+    <>
+      <div className={raleway.className}>
+        <div className="mb-8 mt-8 p-4">
+          <h1 className="text-4xl font-bold text-center text-primary-900 mb-4">
+            Código de vestimenta
+          </h1>
+          <div className="flex flex-col items-center justify-center gap-4">
+            <div>
+              <h2 className="font-semibold text-primary-900 text-2xl mb-4 text-center">
+                Mujeres
+              </h2>
+              <p className="text-primary-900 text-center mb-2 text-lg">
+                Vestido largo tipo coctel, tacones comodos.
+                <br /> Se reserva el uso de estampados y los colores blanco y
+                magenta.
+              </p>
+              <p className="text-primary-900 text-center text-lg">
+                A continuación te colocamos una imagen de referencia para
+                que te inspires:
+              </p>
+            </div>
+            <div>
+              <Image
+                src="https://res.cloudinary.com/dxa54qfxx/image/upload/v1726263127/mujerescropped_gxztzz.png"
+                alt="Mujeres"
+                width={500}
+                height={500}
+                className="mb-4"
+              />
+            </div>
+            <div>
+              <h2 className="font-semibold text-primary-900 text-2xl mb-4 text-center">
+                Hombres
+              </h2>
+              <p className="text-primary-900 text-center mb-2 text-lg">
+                Guayabera manga larga blanca, pantalón en tonalidades tierra
+              </p>
+              <p className="text-primary-900 text-center text-lg">
+                A continuación te colocamos una imagen de referencia para
+                que te inspires:
+              </p>
+            </div>
+            <div>
+              <Image
+                src="https://res.cloudinary.com/dxa54qfxx/image/upload/v1726266023/hombrescropped_kyle2v.png"
+                alt="Mujeres"
+                width={500}
+                height={500}
+                className="mb-4"
+              />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
