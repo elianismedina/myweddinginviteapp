7708e8ba11f03d42cc352c9d53c2fd2b89e5d92c
elianismedina <elianismedina05@outlook.com>
2024-09-13 16:53:51 -0500

    changed dresscode

--- src/app/dresscode.tsx
+++ src/app/dresscode.tsx
@@ -13,21 +13,26 @@ const DresscodeSection = () => {
         <h1 className="text-4xl font-bold text-center text-primary-900 mb-4">
           Código de vestimenta
         </h1>
-        <div className="flex flex-col items-center justify-center">
-          <Image
-            src="https://res.cloudinary.com/dxa54qfxx/image/upload/v1726263127/mujerescropped_gxztzz.png"
-            alt="Mujeres"
-            width={500}
-            height={500}
-            className="mb-4"
-          />
-          <h2 className="font-semibold text-primary-900 text-2xl mb-4">
-            Mujeres
-          </h2>
-          <p className="text-primary-900 text-center">
-            Vestido largo de un solo color, tacones comodos. Se reserva el uso
-            de estampados y los colores blanco y magenta.
-          </p>
+        <div className="flex flex-col items-center justify-center gap-4">
+          <div>
+            <h2 className="font-semibold text-primary-900 text-2xl mb-4 text-center">
+              Mujeres
+            </h2>
+            <p className="text-primary-900 text-center">
+              Vestido largo de un solo color, tacones comodos.
+              <br /> Se reserva el uso de estampados y los colores blanco y
+              magenta.
+            </p>
+          </div>
+          <div>
+            <Image
+              src="https://res.cloudinary.com/dxa54qfxx/image/upload/v1726263127/mujerescropped_gxztzz.png"
+              alt="Mujeres"
+              width={500}
+              height={500}
+              className="mb-4"
+            />
+          </div>
         </div>
       </div>
     </div>
