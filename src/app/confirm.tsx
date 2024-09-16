d77df3209fb99aac49d10cc1ca645b25411e801a
elianismedina <elianismedina05@outlook.com>
2024-09-16 17:23:30 -0500

    changed confim sections

--- src/app/confirm.tsx
+++ src/app/confirm.tsx
@@ -18,9 +18,6 @@ import "react-toastify/dist/ReactToastify.css";
 const raleway = localFont({
   src: "./fonts/Raleway-VariableFont_wght.ttf",
 });
-const greatvibes = localFont({
-  src: "./fonts/GreatVibes-Regular.ttf",
-});
 
 const ConfirmSection = () => {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
@@ -28,118 +25,117 @@ const ConfirmSection = () => {
 
   return (
     <>
-      <div className="flex flex-col items-center justify-center">
-        <div className="text-3xl p-4 text-primary-950">
-          <h1 className={greatvibes.className}>!Nos casamos!</h1>
+      <div className={raleway.className}>
+        <div className="flex flex-col items-center justify-center">
+          <div className="text-3xl p-4 text-primary-950"></div>
+          <Button
+            onPress={onOpen}
+            className=" bg-primary-800 text-primary-100 mb-8 p-4 rounded-md border-4 border-primary-300 font-bold"
+          >
+            Confirma tu asistencia
+          </Button>
         </div>
-        <Button
-          onPress={onOpen}
-          className=" bg-primary-800 text-primary-100 mb-8 p-4 rounded-md border-4 border-primary-100 font-bold"
-        >
-          Confirma tu asistencia
-        </Button>
-      </div>
 
-      <Modal
-        isOpen={isOpen}
-        onOpenChange={onOpenChange}
-        placement="top"
-        backdrop="opaque"
-        motionProps={{
-          variants: {
-            enter: {
-              y: 0,
-              opacity: 1,
-              transition: {
-                duration: 0.3,
-                ease: "easeOut",
+        <Modal
+          isOpen={isOpen}
+          onOpenChange={onOpenChange}
+          placement="top"
+          backdrop="opaque"
+          motionProps={{
+            variants: {
+              enter: {
+                y: 0,
+                opacity: 1,
+                transition: {
+                  duration: 0.3,
+                  ease: "easeOut",
+                },
               },
-            },
-            exit: {
-              y: -20,
-              opacity: 0,
-              transition: {
-                duration: 0.2,
-                ease: "easeIn",
+              exit: {
+                y: -20,
+                opacity: 0,
+                transition: {
+                  duration: 0.2,
+                  ease: "easeIn",
+                },
               },
+              // transition: "all 0.3s",
             },
-            // transition: "all 0.3s",
-          },
-        }}
-        className={raleway.className}
-      >
-        <ToastContainer position="top-center" autoClose={1000} />
-        <ModalContent>
-          {(onClose) => (
-            <>
-              <form
-                action={async (formData) => {
-                  const { error } = await sendEmail(formData);
-                  if (error) {
-                    console.error(error);
-                    return;
-                  }
-                }}
-              >
-                <ModalHeader className="flex flex-col gap-1 text-primary-950">
-                  Confirma tu asistencia
-                </ModalHeader>
-                <ModalBody>
-                  <div className="flex flex-col gap-4">
-                    <label htmlFor="name" className="text-primary-950">
-                      Nombre
-                    </label>
-                    <input
-                      type="text"
-                      required
-                      maxLength={50}
-                      name="senderName"
-                      placeholder="Jane Doe"
-                      id="name"
-                      className="p-2 bg-primary-950 text-primary-100"
-                    />
-                  </div>
-                  <div className="flex flex-col gap-4">
-                    <label htmlFor="phone" className="text-primary-950">
-                      Número de teléfono
-                    </label>
-                    <input
-                      type="phone"
-                      required
-                      maxLength={10}
-                      placeholder="1234567890"
-                      name="senderPhone"
-                      id="phone"
-                      className="p-2 bg-primary-950 text-primary-100"
-                    />
-                  </div>
-                </ModalBody>
-                <ModalFooter>
-                  <Button
-                    color="danger"
-                    variant="light"
-                    onPress={onClose}
-                    className="px-4 py-2 text-primary-400 bg-primary-950 rounded-md flex flex-row items-center justify-center gap-2 text-sm"
-                  >
-                    Cerrar
-                  </Button>
-                  <div>
-                    <button
-                      type="submit"
-                      className="px-4 py-2 text-primary-950 bg-primary-500 font-bold rounded-md flex flex-row items-center justify-center gap-2 text-sm"
-                      onClick={() => {
-                        notify();
-                      }}
+          }}
+        >
+          <ToastContainer position="top-center" autoClose={1000} />
+          <ModalContent>
+            {(onClose) => (
+              <>
+                <form
+                  action={async (formData) => {
+                    const { error } = await sendEmail(formData);
+                    if (error) {
+                      console.error(error);
+                      return;
+                    }
+                  }}
+                >
+                  <ModalHeader className="flex flex-col gap-1 text-primary-950">
+                    Confirma tu asistencia
+                  </ModalHeader>
+                  <ModalBody>
+                    <div className="flex flex-col gap-4">
+                      <label htmlFor="name" className="text-primary-950">
+                        Nombre
+                      </label>
+                      <input
+                        type="text"
+                        required
+                        maxLength={50}
+                        name="senderName"
+                        placeholder="Jane Doe"
+                        id="name"
+                        className="p-2 bg-primary-950 text-primary-100"
+                      />
+                    </div>
+                    <div className="flex flex-col gap-4">
+                      <label htmlFor="phone" className="text-primary-950">
+                        Número de teléfono
+                      </label>
+                      <input
+                        type="phone"
+                        required
+                        maxLength={10}
+                        placeholder="1234567890"
+                        name="senderPhone"
+                        id="phone"
+                        className="p-2 bg-primary-950 text-primary-100"
+                      />
+                    </div>
+                  </ModalBody>
+                  <ModalFooter>
+                    <Button
+                      color="danger"
+                      variant="light"
+                      onPress={onClose}
+                      className="px-4 py-2 text-primary-400 bg-primary-950 rounded-md flex flex-row items-center justify-center gap-2 text-sm"
                     >
-                      Confirmar asistencia
-                    </button>
-                  </div>
-                </ModalFooter>
-              </form>
-            </>
-          )}
-        </ModalContent>
-      </Modal>
+                      Cerrar
+                    </Button>
+                    <div>
+                      <button
+                        type="submit"
+                        className="px-4 py-2 text-primary-950 bg-primary-500 font-bold rounded-md flex flex-row items-center justify-center gap-2 text-sm"
+                        onClick={() => {
+                          notify();
+                        }}
+                      >
+                        Confirmar asistencia
+                      </button>
+                    </div>
+                  </ModalFooter>
+                </form>
+              </>
+            )}
+          </ModalContent>
+        </Modal>
+      </div>
     </>
   );
 };
