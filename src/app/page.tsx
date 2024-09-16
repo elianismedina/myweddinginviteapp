31b4e78a98014452f23d10d9781e4a5fe4738da7
elianismedina <elianismedina05@outlook.com>
2024-09-15 23:00:32 -0500

    added sugerencias section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import DresscodeSection from "./dresscode";
 import GiftSection from "./gift";
 import MensajeSection from "./mensaje";
 import RecepcionSection from "./recepcion";
+import SugerenciasSection from "./sugerencias";
 import TimelineSection from "./timeline";
 import TimerSection from "./timer";
 import VideoSection from "./video";
@@ -22,6 +23,7 @@ export default function Home() {
         <TimelineSection />
         <DresscodeSection />
         <GiftSection />
+        <SugerenciasSection />
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center bg-primary-950 w-full text-primary-400 p-2 text-sm">
         <p>Carlos & Elizabeth wedding invite</p>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,39 +3,45 @@ import type { Config } from "tailwindcss";
 const { nextui } = require("@nextui-org/theme");
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/components/modal.js",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        playfairdisplay: "Playfair Display, ui-serif",
-      },
-      colors: {
-        transparent: "transparent",
-        current: "currentColor",
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        primary: {
-          50: "#FFFEFC",
-          100: "#FFFDFA",
-          200: "#FFFBF5",
-          300: "#FFF8F0",
-          400: "#FFEEE3",
-          500: "#FFD4C5",
-          600: "#E6BEAE",
-          700: "#BF8B78",
-          800: "#995E4D",
-          900: "#733B2C",
-          950: "#4A1D12",
-        },
-      },
-    },
+  	extend: {
+  		fontFamily: {
+  			playfairdisplay: 'Playfair Display, ui-serif'
+  		},
+  		colors: {
+  			transparent: 'transparent',
+  			current: 'currentColor',
+  			background: 'var(--background)',
+  			foreground: 'var(--foreground)',
+  			primary: {
+  				'50': '#FFFEFC',
+  				'100': '#FFFDFA',
+  				'200': '#FFFBF5',
+  				'300': '#FFF8F0',
+  				'400': '#FFEEE3',
+  				'500': '#FFD4C5',
+  				'600': '#E6BEAE',
+  				'700': '#BF8B78',
+  				'800': '#995E4D',
+  				'900': '#733B2C',
+  				'950': '#4A1D12'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [nextui()],
+  plugins: [nextui(), require("tailwindcss-animate")],
 };
 darkMode: "class";
 export default config;
