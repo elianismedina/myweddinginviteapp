d42e6da9d035e30cc16280e4032d78300360fb10
elianismedina <elianismedina05@outlook.com>
2024-09-17 20:54:55 -0500

    additional changes

--- src/app/ceremonia.tsx
+++ src/app/ceremonia.tsx
@@ -18,18 +18,18 @@ const CeremoniaSection = () => {
             Ceremonia
           </h1>
           <div className="flex flex-col gap-2 mb-6">
-            <h2 className="text-lg text-center text-primary-950 font-semibold">
+            <h2 className="text-md md:text-lg text-center text-primary-950 font-semibold">
               Lugar:
             </h2>
-            <p className="text-lg text-center text-primary-950">
+            <p className="text-md md:text-lg text-center text-primary-950">
               Iglesia Catedral de <br /> Santa Catalina de Alejandría
             </p>
           </div>
           <div className="flex flex-col gap-2 mb-6">
-            <h2 className="text-lg text-center text-primary-950 font-semibold">
+            <h2 className="text-md md:text-lg text-center text-primary-950 font-semibold">
               Dirección:
             </h2>
-            <p className="text-lg text-center text-primary-950">
+            <p className="text-md md:text-lg text-center text-primary-950">
               Calle de los Santos de Piedra,
               <br /> Cra. 4, Cartagena, Bolívar
             </p>
@@ -46,11 +46,11 @@ const CeremoniaSection = () => {
             loading="lazy"
           />
         </div>
-        <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center justify-center mt-4">
           <FaMapLocationDot className="text-4xl text-primary-950" />
           <Link
             href="https://maps.app.goo.gl/U7cmhhMcFryFT67w8"
-            className="flex items-center justify-center p-4 text-lg text-primary-900 font-semibold underline"
+            className="flex items-center justify-center p-2 text-md md:text-lg text-primary-900 font-semibold underline"
             target="_blank"
           >
             Toca para ir a la ubicación
