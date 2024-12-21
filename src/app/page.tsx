b3483a8bcb1aa6bf94476f921a41df0c21afd918
elianismedina <elianismedina05@outlook.com>
2024-12-21 06:54:00 -0500

    removed timer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import CeremoniaSection from "./ceremonia";
-import ConfirmSection from "./confirm";
+
 import DresscodeSection from "./dresscode";
 import GiftSection from "./gift";
 import MensajeSection from "./mensaje";
@@ -8,7 +8,7 @@ import RecepcionSection from "./recepcion";
 import ShareSection from "./share";
 import SugerenciasSection from "./sugerencias";
 import TimelineSection from "./timeline";
-import TimerSection from "./timer";
+
 import VideoSection from "./video";
 
 export default function Home() {
@@ -16,8 +16,6 @@ export default function Home() {
     <div className="flex flex-col items-center justify-items-center min-h-screen gap-16]">
       <main>
         <VideoSection />
-        <TimerSection />
-        <ConfirmSection />
         <MensajeSection />
         <CeremoniaSection />
         <RecepcionSection />
